32ebbf10a7bc171db419bfc4c10db4622d76893b
MarkThaValor <geniusmark99@gmail.com>
2024-08-23 23:54:58 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,9 +106,8 @@ export default function Home() {
 
 
 
-          <p className="text-xl md:text-2xl mb-8 text-center my-10 text-slate-400">
+          <p className="text-sm md:text-xl lg:text-2xl mb-8 text-center my-10 text-slate-400">
             Join our waiting list to be the first to know when we launch.
-
 
           </p>
 
@@ -128,7 +127,7 @@ export default function Home() {
 
 
         </div>
-        <div className="w-full absolute bottom-[100px] gap-y-2 justify-center items-center">
+        <div className="w-full absolute bottom-4 lg:bottom-[100px] gap-y-2 justify-center items-center">
 
           <Swiper
             spaceBetween={5}
@@ -151,7 +150,7 @@ export default function Home() {
             <SwiperSlide>
 
               <div className='w-full flex justify-center text-center items-center h-[20vh]'>
-                <button className='text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-blue-500/20 rounded-full py-1 cursor-pointer hover:bg-blue-500/25 transition-all px-4 border border-blue-500 text-blue-400'>
+                <button className='text-xs sm:text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-blue-500/20 rounded-full py-1 cursor-pointer hover:bg-blue-500/25 transition-all px-4 border border-blue-500 text-blue-400'>
 
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="size-[1rem] fill-blue-500">
                     <path d="M7.5 5C6.12 5 5 6.12 5 7.5L5 8L5 40L5 40.5C5 41.88 6.12 43 7.5 43L8 43L21.519531 43C20.849531 42.13 20.380391 41.11 20.150391 40L11 40C11 38.343 9.657 37 8 37L8 11C9.657 11 11 9.657 11 8L20 8C20 9.657 21.343 11 23 11L23 14.519531C23.87 13.849531 24.89 13.380391 26 13.150391L26 8L26 7.5C26 6.12 24.88 5 23.5 5L23 5L8 5L7.5 5 z M 15.5 11.5 A 2 2 0 0 0 15.5 15.5 A 2 2 0 0 0 15.5 11.5 z M 27.5 16C25.032499 16 23 18.032499 23 20.5L23 38.5C23 40.967501 25.032499 43 27.5 43L39.5 43C41.967501 43 44 40.967501 44 38.5L44 20.5C44 18.032499 41.967501 16 39.5 16L27.5 16 z M 15.5 19C12.46 19 10 21.24 10 24C10 26.76 12.46 29 15.5 29C17.36 29 19.01 28.159141 20 26.869141L20 21.130859C19.01 19.840859 17.36 19 15.5 19 z M 27.5 19L34 19L34 40L27.5 40C26.653501 40 26 39.346499 26 38.5L26 20.5C26 19.653501 26.653501 19 27.5 19 z M 37 19L39.5 19C40.346499 19 41 19.653501 41 20.5L41 38.5C41 39.346499 40.346499 40 39.5 40L37 40L37 19 z M 15.5 22C16.86 22 18 22.92 18 24C18 25.08 16.86 26 15.5 26C14.14 26 13 25.08 13 24C13 22.92 14.14 22 15.5 22 z M 15.5 32.5 A 2 2 0 0 0 15.5 36.5 A 2 2 0 0 0 15.5 32.5 z" />
@@ -167,7 +166,7 @@ export default function Home() {
             <SwiperSlide>
 
               <div className='w-full flex justify-center text-center items-center h-[20vh]'>
-                <button className='text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-emerald-500/20 rounded-full py-1 cursor-pointer hover:bg-emerald-500/25 transition-all px-4 border border-emerald-500 text-emerald-400'>
+                <button className='text-xs sm:text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-emerald-500/20 rounded-full py-1 cursor-pointer hover:bg-emerald-500/25 transition-all px-4 border border-emerald-500 text-emerald-400'>
 
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="size-[1rem] fill-emerald-500">
                     <path d="M11.5 4C7.4 4 4 7.4 4 11.5C4 15.6 7.4 19 11.5 19C15.6 19 19 15.6 19 11.5C19 7.4 15.6 4 11.5 4 z M 39.960938 5.9804688 A 2.0002 2.0002 0 0 0 39.740234 6L32 6 A 2.0002 2.0002 0 1 0 32 10L35.171875 10L30.171875 15L27 15 A 2.0002 2.0002 0 0 0 25.285156 15.970703L22.867188 20L19 20 A 2.0002 2.0002 0 0 0 17.285156 20.970703L14.867188 25L11 25 A 2.0002 2.0002 0 0 0 9.2851562 25.970703L6.2851562 30.970703 A 2.0002 2.0002 0 1 0 9.7148438 33.029297L12.132812 29L16 29 A 2.0002 2.0002 0 0 0 17.714844 28.029297L20.132812 24L24 24 A 2.0002 2.0002 0 0 0 25.714844 23.029297L28.132812 19L31 19 A 2.0002 2.0002 0 0 0 32.414062 18.414062L38 12.828125L38 16 A 2.0002 2.0002 0 1 0 42 16L42 8.2441406 A 2.0002 2.0002 0 0 0 39.960938 5.9804688 z M 12.025391 6.9609375C12.250391 6.95625 12.474219 7.0492188 12.699219 7.1992188C12.899219 7.3992188 13 7.7 13 8L13 15C13 15.6 12.6 16 12 16C11.4 16 11 15.6 11 15L11 9.3007812L10.199219 9.5C9.6992188 9.6 9.2 9.2992188 9 8.6992188C8.9 8.1992188 9.2007812 7.7 9.8007812 7.5L11.800781 7C11.875781 6.975 11.950391 6.9625 12.025391 6.9609375 z M 39.970703 20.972656 A 2.0002 2.0002 0 0 0 38 23L38 40 A 2.0002 2.0002 0 1 0 42 40L42 23 A 2.0002 2.0002 0 0 0 39.970703 20.972656 z M 31.970703 22.972656 A 2.0002 2.0002 0 0 0 30 25L30 40 A 2.0002 2.0002 0 1 0 34 40L34 25 A 2.0002 2.0002 0 0 0 31.970703 22.972656 z M 23.970703 26.972656 A 2.0002 2.0002 0 0 0 22 29L22 40 A 2.0002 2.0002 0 1 0 26 40L26 29 A 2.0002 2.0002 0 0 0 23.970703 26.972656 z M 15.970703 31.972656 A 2.0002 2.0002 0 0 0 14 34L14 40 A 2.0002 2.0002 0 1 0 18 40L18 34 A 2.0002 2.0002 0 0 0 15.970703 31.972656 z M 7.9707031 36.972656 A 2.0002 2.0002 0 0 0 6 39L6 40 A 2.0002 2.0002 0 1 0 10 40L10 39 A 2.0002 2.0002 0 0 0 7.9707031 36.972656 z" />
@@ -185,7 +184,7 @@ export default function Home() {
             <SwiperSlide>
 
               <div className='w-full flex justify-center text-center items-center h-[20vh]'>
-                <button className='text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-rose-500/20 rounded-full py-1 cursor-pointer hover:bg-rose-500/25 transition-all px-4 border border-rose-500 text-rose-400'>
+                <button className='text-xs sm:text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-rose-500/20 rounded-full py-1 cursor-pointer hover:bg-rose-500/25 transition-all px-4 border border-rose-500 text-rose-400'>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="size-[1rem] fill-rose-500">
                     <path d="M9.5273438 5 A 1.50015 1.50015 0 0 0 8 6.5L8 37.5C8 40.519774 10.480226 43 13.5 43L34.5 43C37.519774 43 40 40.519774 40 37.5L40 6.5 A 1.50015 1.50015 0 0 0 37.890625 5.1289062L33.96875 6.8730469L29.556641 5.1074219 A 1.50015 1.50015 0 0 0 28.443359 5.1074219L24 6.8847656L19.556641 5.1074219 A 1.50015 1.50015 0 0 0 18.443359 5.1074219L14.03125 6.8730469L10.109375 5.1289062 A 1.50015 1.50015 0 0 0 9.5273438 5 z M 19 8.1152344L23.443359 9.8925781 A 1.50015 1.50015 0 0 0 24.556641 9.8925781L29 8.1152344L33.443359 9.8925781 A 1.50015 1.50015 0 0 0 34.609375 9.8710938L37 8.8085938L37 37.5C37 38.898226 35.898226 40 34.5 40L13.5 40C12.101774 40 11 38.898226 11 37.5L11 8.8085938L13.390625 9.8710938 A 1.50015 1.50015 0 0 0 14.556641 9.8925781L19 8.1152344 z M 15.5 17 A 1.50015 1.50015 0 1 0 15.5 20L32.5 20 A 1.50015 1.50015 0 1 0 32.5 17L15.5 17 z M 15.5 26 A 1.50015 1.50015 0 1 0 15.5 29L24.5 29 A 1.50015 1.50015 0 1 0 24.5 26L15.5 26 z M 30.5 26 A 1.50015 1.50015 0 1 0 30.5 29L32.5 29 A 1.50015 1.50015 0 1 0 32.5 26L30.5 26 z M 15.5 32 A 1.50015 1.50015 0 1 0 15.5 35L24.5 35 A 1.50015 1.50015 0 1 0 24.5 32L15.5 32 z M 30.5 32 A 1.50015 1.50015 0 1 0 30.5 35L32.5 35 A 1.50015 1.50015 0 1 0 32.5 32L30.5 32 z" />
                   </svg>
@@ -201,7 +200,7 @@ export default function Home() {
             <SwiperSlide>
 
               <div className='w-full flex justify-center text-center items-center h-[20vh]'>
-                <button className='text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-teal-500/20 rounded-full py-1 cursor-pointer hover:bg-teal-500/25 transition-all px-4 border border-teal-500 text-teal-400'>
+                <button className='text-xs sm:text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-teal-500/20 rounded-full py-1 cursor-pointer hover:bg-teal-500/25 transition-all px-4 border border-teal-500 text-teal-400'>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="size-[1rem] fill-teal-500">
                     <path d="M3.5 6 A 1.50015 1.50015 0 1 0 3.5 9L6.2558594 9C6.9837923 9 7.5905865 9.5029243 7.7285156 10.21875L8.0273438 11.78125L11.251953 28.716797C11.835068 31.772321 14.527135 34 17.638672 34L36.361328 34C39.472865 34 42.166064 31.773177 42.748047 28.716797L45.972656 11.78125 A 1.50015 1.50015 0 0 0 44.5 10L10.740234 10L10.675781 9.6582031C10.272657 7.5455321 8.4069705 6 6.2558594 6L3.5 6 z M 11.3125 13L42.6875 13L39.800781 28.15625C39.484764 29.81587 38.051791 31 36.361328 31L17.638672 31C15.948808 31 14.516781 29.8158 14.199219 28.15625L14.199219 28.154297L11.3125 13 z M 23.5 16 A 1.50015 1.50015 0 1 0 23.5 19L31.5 19 A 1.50015 1.50015 0 1 0 31.5 16L23.5 16 z M 20 36 A 3 3 0 0 0 20 42 A 3 3 0 0 0 20 36 z M 34 36 A 3 3 0 0 0 34 42 A 3 3 0 0 0 34 36 z" />
                   </svg>
@@ -218,7 +217,7 @@ export default function Home() {
             <SwiperSlide>
 
               <div className='w-full flex justify-center text-center items-center h-[20vh]'>
-                <button className='text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-amber-500/20 rounded-full py-1 cursor-pointer hover:bg-amber-500/25 transition-all px-4 border border-amber-500 text-amber-400'>
+                <button className='text-xs sm:text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-amber-500/20 rounded-full py-1 cursor-pointer hover:bg-amber-500/25 transition-all px-4 border border-amber-500 text-amber-400'>
 
 
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="size-[1rem] fill-amber-500">
@@ -236,7 +235,7 @@ export default function Home() {
             <SwiperSlide>
 
               <div className='w-full flex justify-center text-center items-center h-[20vh]'>
-                <button className='text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-pink-500/20 rounded-full py-1 cursor-pointer hover:bg-pink-500/25 transition-all px-4 border border-pink-500 text-pink-400'>
+                <button className='text-xs sm:text-sm font-semibold md:text-base outline-none flex gap-x-2 items-center  bg-pink-500/20 rounded-full py-1 cursor-pointer hover:bg-pink-500/25 transition-all px-4 border border-pink-500 text-pink-400'>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="size-[1rem] fill-pink-500">
                     <path d="M3.4667969 3.9863281 A 1.50015 1.50015 0 0 0 2.5625 6.671875L7.5625 10.671875 A 1.5007322 1.5007322 0 1 0 9.4375 8.328125L4.4375 4.328125 A 1.50015 1.50015 0 0 0 3.4667969 3.9863281 z M 44.488281 3.9863281 A 1.50015 1.50015 0 0 0 43.5625 4.328125L38.5625 8.328125 A 1.5007322 1.5007322 0 1 0 40.4375 10.671875L45.4375 6.671875 A 1.50015 1.50015 0 0 0 44.488281 3.9863281 z M 24 4C15.733378 4 9 10.733378 9 19L9 27.185547L6.2382812 33.498047C5.5384109 35.098789 6.7819446 37 8.5292969 37L18 37C18 40.295865 20.704135 43 24 43C27.295865 43 30 40.295865 30 37L39.470703 37C41.218055 37 42.461589 35.098789 41.761719 33.498047L39 27.185547L39 19C39 10.733378 32.266622 4 24 4 z M 24 7C30.645378 7 36 12.354622 36 19L36 27.5 A 1.50015 1.50015 0 0 0 36.125 28.101562L38.707031 34L24 34L9.2929688 34L11.875 28.101562 A 1.50015 1.50015 0 0 0 12 27.5L12 19C12 12.354622 17.354622 7 24 7 z M 1.5 16 A 1.50015 1.50015 0 1 0 1.5 19L5.5 19 A 1.50015 1.50015 0 1 0 5.5 16L1.5 16 z M 42.5 16 A 1.50015 1.50015 0 1 0 42.5 19L46.5 19 A 1.50015 1.50015 0 1 0 46.5 16L42.5 16 z M 21 37L24 37L27 37C27 38.674135 25.674135 40 24 40C22.325865 40 21 38.674135 21 37 z" />
                   </svg>
